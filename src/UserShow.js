cc34b04608767f3775fed0b332658c8f2fcc61a4
nadafruksana <42529671+nadafruksana@users.noreply.github.com>
2020-04-06 21:58:31 +0530

    Update UserShow.js

--- src/UserShow.js
+++ src/UserShow.js
@@ -1,30 +1,42 @@
 import React from 'react'
 import { connect } from 'react-redux'
-//import {startGetUsers} from './actions/usersAction'
-class UserShow extends React.Component {
-    
-    render() {
-        return (
-            <div>
-                {
-                    this.props.user ? ( <div> 
-                        <h2>User Show - {this.props.match.params.id}</h2>
-                        <p> {this.props.user.name} - {this.props.user.email}</p> 
-                    </div> ) : (
-                        <p> loading....</p>
-                            //{this.props.dispatch(startGetUsers())}
-                    )
-                }
-                
-            </div>
-        )
+import { Link } from 'react-router-dom'
+
+class UserShow extends React.Component{
+    constructor(){
+        super()
+        this.state={
+            user:{}
+        }
+    }
+  
+   render(){
+    return(
+        <div>
+            {
+                this.props.user ? (<div>
+                    <h2> POST WRITTEN BY USER - {this.props.user.name}</h2>
+                    <ul> 
+                        {this.props.userPosts.map(post=>{
+                            return <li key={post.id}><Link to={`/posts/${post.id}`}>{post.title}</Link></li>
+                        })
+                    }
+                                </ul></div>): (
+                                   <div> <p> loading ....</p> </div>
+                                )
+            }
+            
+        </div>
+    )
+
+}
+}
+const mapStateToProps=(state, props)=>{
+    return {
+        user:state.users.find(user => user.id== props.match.params.id),
+        userPosts:state.posts.filter(post=> post.userId== props.match.params.id)   
     }
 }
 
-const mapStateToProps = (state, props) => {
-    return {
-        user: state.users.find(user => user.id == props.match.params.id)
-    }
-}
 export default connect(mapStateToProps)(UserShow)
 
