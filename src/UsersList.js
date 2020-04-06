4cabbafd1b6cf0b30704ccbb3fa003e81fe7fae0
nadafruksana <42529671+nadafruksana@users.noreply.github.com>
2020-04-06 21:54:58 +0530

    Update UsersList.js

--- src/UsersList.js
+++ src/UsersList.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import { startGetUsers } from './actions/usersAction'
-// npm install axios 
 import { Link } from 'react-router-dom'
 
 class UsersList extends React.Component {
@@ -34,87 +33,14 @@ const mapStateToProps = (state) => {
 export default connect(mapStateToProps)(UsersList)
 
 
-// import React from 'react'
-// // npm install axios 
-// import axios from 'axios'
-// import { Link } from 'react-router-dom'
-
-// class UsersList extends React.Component {
-//     constructor() {
-//         super()
-//         this.state = {
-//             users: []
-//         }
-//     }
-
-//     componentDidMount() {
-//         axios.get('http://jsonplaceholder.typicode.com/users')
-//             .then((response) => {
-//                 // console.log(response)
-//                 const users = response.data
-//                 this.setState({ users })
-//             })
-//             .catch((err) => {
-//                 console.log(err)
-//             })
-//     }
-
-//     render() {
-//         return (
-//             <div>
-//                 <h1>Listing Users - {this.state.users.length}</h1>
-//                 <ul>
-//                     {this.state.users.map(user => {
-//                         return <li key={user.id}><Link to={`/users/${user.id}`}> {user.name} </Link> </li>
-//                     })}
-//                 </ul>
-//             </div>
-//         )
-//     }
-// }
-
-// export default UsersList
 
 
-// import React from 'react'
-// import axios from 'axios'
-// import {Link} from 'react-router-dom'
 
-// class UsersList extends React.Component {
-//     constructor() {
-//         console.log("constructor")
-//         super() 
-//         this.state = {
-//             users:[] 
-//         }
-//     }
-//     componentDidMount(){
-//         axios.get('https://jsonplaceholder.typicode.com/users')
-//         .then((response) => {
-//             //console.log(response)
-//             const users = response.data
-//             this.setState({users})
-//         })
-//         .catch((err)=>{
-//             console.log(err)
-//         })
-// }
 
-//         render() {
-//         console.log('render')
-//         return (
-//             <div> 
-                 
-//                  <h2>listing Users{this.state.users.length}</h2>
-//                  <ul>
-//                      {this.state.users.map(user =>{
-//                          return <li key={user.id}><Link to={`/users/${user.id}`}>
-//                              {user.name}</Link></li>
-//                      })}
-//                  </ul>
-//             </div> 
-//         )
-//     }
-// }
 
-// export default UsersList
+
+
+
+
+
+
